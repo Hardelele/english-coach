1ae2b263dd588f9a0d0c631730733486512a1bac
Hardelele <i.lapshina@mail.ru>
2019-11-28 01:16:26 +0300

    Question: add methods and variables - add russianWordAnswer - make options as private variable - add follow list of methods:   setRandomOptions()   setupQuestion()   setEnglishWordAnswer()   getOptions()   setRussianWordAnswer() - fixed checkAnswer() method

--- src/main/java/com/github/hardelele/englishcoach/Question.java
+++ src/main/java/com/github/hardelele/englishcoach/Question.java
@@ -1,13 +1,55 @@
 package com.github.hardelele.englishcoach;
 
+import java.util.*;
+
 public class Question {
 
-    String queriedWord;
+    private String englishWordAnswer;
 
-    String[] options = new String[4];
+    private String russianWordAnswer;
 
-    public boolean checkAnswer(int receivedAnswerId, String correctAnswer) {
-        return options[receivedAnswerId].equals(correctAnswer);
+    private String[] options = new String[4];
+
+    public void setRandomOptions(HashMap<String,String> wordBook) {
+        ArrayList<String> translatesList = new ArrayList<String>();
+
+        for (String value : wordBook.values()) {
+            translatesList.add(value);
+        }
+
+        Collections.shuffle(translatesList);
+        translatesList.remove(wordBook.get(englishWordAnswer));
+
+        for (int counter = 0; counter < 4; counter++) {
+            options[counter] = translatesList.get(counter);
+        }
     }
 
+    public void setupQuestion(HashMap<String,String> wordBook, String englishWordAnswer) {
+        setEnglishWordAnswer(englishWordAnswer);
+        setRussianWordAnswer(wordBook.get(englishWordAnswer));
+
+        HashMap<String, String> cloneOfWordBook = (HashMap<String, String>) wordBook.clone();
+        cloneOfWordBook.remove(englishWordAnswer);
+        setRandomOptions(cloneOfWordBook);
+
+        Random generator = new Random();
+        options[generator.nextInt(4)] = wordBook.get(englishWordAnswer);
+    }
+
+    public void setEnglishWordAnswer(String englishWordAnswer) {
+        this.englishWordAnswer = englishWordAnswer;
+    }
+
+    public boolean checkAnswer(int receivedAnswerId) {
+        return options[receivedAnswerId].equals(russianWordAnswer);
+    }
+
+    public String[] getOptions() {
+        return options;
+    }
+
+    public void setRussianWordAnswer(String russianWordAnswer) {
+        this.russianWordAnswer = russianWordAnswer;
+    }
 }
