4ec40d5e09f6888993d84dc21386d9203c82d4ac
Hardelele <i.lapshina@mail.ru>
2019-11-27 16:23:29 +0300

    Add Application.java and implemented Call Executor constructor and transfer application under his control

---
+++ src/main/java/com/github/hardelele/englishcoach/Application.java
@@ -0,0 +1,11 @@
+package com.github.hardelele.englishcoach;
+
+    /*
+    TODO:: Load words from file >> Make HashMap of words >>
+     */
+
+public class Application {
+    public static void main(String[] args) {
+        Executor executor = new Executor();
+    }
+}
