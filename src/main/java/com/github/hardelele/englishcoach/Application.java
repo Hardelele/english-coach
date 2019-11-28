1318cdcb9395790e76774a3619494fe40e27ab92
Hardelele <i.lapshina@mail.ru>
2019-11-28 19:09:00 +0300

    Change menu to executor

--- src/main/java/com/github/hardelele/englishcoach/Application.java
+++ src/main/java/com/github/hardelele/englishcoach/Application.java
@@ -6,6 +6,6 @@ package com.github.hardelele.englishcoach;
 
 public class Application {
     public static void main(String[] args) {
-        Menu menu = new Menu();
+        Executor executor = new Executor();
     }
 }
