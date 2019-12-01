f1f43dc2f9ab74c9dc69541c92c08970acc4e2a3
Hardelele <i.lapshina@mail.ru>
2019-12-01 03:22:30 +0300

    WordsSerializer: add parseFromTxt method - Add parseFromTxt method that could parse words from txt file

--- src/main/java/com/github/hardelele/englishcoach/WordsSerializer.java
+++ src/main/java/com/github/hardelele/englishcoach/WordsSerializer.java
@@ -2,6 +2,7 @@ package com.github.hardelele.englishcoach;
 
 import java.io.*;
 import java.util.HashMap;
+import java.util.StringTokenizer;
 
 public class WordsSerializer {
     public static void save(HashMap<String,String> wordbook, String fileName) throws IOException {
@@ -16,6 +17,37 @@ public class WordsSerializer {
         ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
         HashMap<String,String> wordbook = (HashMap<String,String>) objectInputStream.readObject();
         objectInputStream.close();
+        return wordbook;
+    }
+
+    public static HashMap<String,String> parseFromTxt(String fileName) throws IOException {
+
+        HashMap<String,String> wordbook = new HashMap<>();
+
+        File inputFile = new File(fileName);
+
+        if(!inputFile.exists()) {
+            inputFile.createNewFile();
+        }
+
+        FileReader fileReader = new FileReader(inputFile);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+        String currentLine = bufferedReader.readLine();
+
+        StringTokenizer tokenizer;
+
+        while (currentLine != null) {
+            tokenizer = new StringTokenizer(currentLine,"::");
+
+            String word = tokenizer.nextToken();
+            String translate = tokenizer.nextToken();
+
+            wordbook.put(word,translate);
+
+            currentLine = bufferedReader.readLine();
+        }
+
         return wordbook;
     }
 }
